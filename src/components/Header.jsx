a54954b07a591233e277dc3b2c6d75fbd8ae89eb
Almira <almira@healyz.com>
2025-10-06 19:21:27 -0400

    Fix My Account link in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -81,7 +81,7 @@ const Header = () => {
                 {isUserMenuOpen && (
                   <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50">
                     <Link
-                      to="/account"
+                      to="/my-account"
                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                       onClick={() => setIsUserMenuOpen(false)}
                     >
